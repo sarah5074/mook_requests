7a5cc003ab38f220c6c134b8001233d9d42b1edd
507431412@qq.com <507431412@qq.com>
2017-12-19 18:29:22 +0800

    add_more

--- base/test_method.py
+++ base/test_method.py
@@ -3,8 +3,9 @@ from base.demo import RunMain
 #创建一个类继承unittest
 class TestMethod(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self):#，每个case执行之前都要执行setup
         self.run = RunMain()
+        #self.user_id = self.test_001()
 
     def test_001(self):
         url = 'http://127.0.0.1:8000/login/'
@@ -14,17 +15,23 @@ class TestMethod(unittest.TestCase):
         #print(res)
         res = self.run.run_main(url,"POST",data)
         self.assertEqual(res['user'],'sarah')
+        globals()['userid'] = 10034   #定义全局变量
+        #return self.user_id
         #print(res)
 
+    @unittest.skip('test_002')    #跳过test_002的方法
     def test_002(self):
         url = 'http://127.0.0.1:8000/login/'
         data = {'username':'sarah',
         'password':'fssgsgsgsg'}
         res = self.run.run_main(url,"POST",data)
         print(res)
+        print(userid)
 
     def tearDown(self):
         pass
 
 if __name__ =='__main__':
-    unittest.main()
+    suite = unittest.TestSuite()#创建测试集
+    suite.addTest(TestMethod('test_001'))
+    unittest.run(suite)
